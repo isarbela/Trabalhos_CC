24b0fa4d6e55df95966e1d449d1e0acc1fe83d88
gabrielpenajo <gabrielpenajo13@gmail.com>
2023-05-26 07:06:23 -0300

    não sei se commito
    Co-authored-by: Isabela V. Magalhães <isarbela@users.noreply.github.com>
    Co-authored-by: Matheus Mattioli <matheus.mattioli@hotmail.com>

---
+++ compilador/Analisador.py
@@ -0,0 +1,153 @@
+# Generated from compilador/Analisador.g4 by ANTLR 4.7.2
+from antlr4 import *
+from io import StringIO
+from typing.io import TextIO
+import sys
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\16")
+        buf.write("\u00b8\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+        buf.write("\4\16\t\16\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2")
+        buf.write("\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3")
+        buf.write("\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2")
+        buf.write("\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3")
+        buf.write("\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2")
+        buf.write("\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\5\2h\n\2\3\3\5\3")
+        buf.write("k\n\3\3\3\6\3n\n\3\r\3\16\3o\3\4\5\4s\n\4\3\4\6\4v\n\4")
+        buf.write("\r\4\16\4w\3\4\3\4\6\4|\n\4\r\4\16\4}\5\4\u0080\n\4\3")
+        buf.write("\5\3\5\7\5\u0084\n\5\f\5\16\5\u0087\13\5\3\6\3\6\3\6\7")
+        buf.write("\6\u008c\n\6\f\6\16\6\u008f\13\6\3\6\3\6\3\7\3\7\3\7\3")
+        buf.write("\b\3\b\7\b\u0098\n\b\f\b\16\b\u009b\13\b\3\b\5\b\u009e")
+        buf.write("\n\b\3\b\3\b\3\b\3\t\3\t\3\t\3\n\3\n\3\n\3\n\3\n\3\n\3")
+        buf.write("\n\3\n\3\n\5\n\u00af\n\n\3\13\3\13\3\f\3\f\3\r\3\r\3\16")
+        buf.write("\3\16\2\2\17\3\3\5\4\7\5\t\6\13\7\r\2\17\b\21\t\23\n\25")
+        buf.write("\13\27\f\31\r\33\16\3\2\t\4\2--//\4\2C\\c|\5\2\62;C\\")
+        buf.write("c|\4\2))^^\4\2\f\f\17\17\5\2\13\f\17\17\"\"\5\2,-//\61")
+        buf.write("\61\2\u00d4\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2\2\2\2\t\3")
+        buf.write("\2\2\2\2\13\3\2\2\2\2\17\3\2\2\2\2\21\3\2\2\2\2\23\3\2")
+        buf.write("\2\2\2\25\3\2\2\2\2\27\3\2\2\2\2\31\3\2\2\2\2\33\3\2\2")
+        buf.write("\2\3g\3\2\2\2\5j\3\2\2\2\7r\3\2\2\2\t\u0081\3\2\2\2\13")
+        buf.write("\u0088\3\2\2\2\r\u0092\3\2\2\2\17\u0095\3\2\2\2\21\u00a2")
+        buf.write("\3\2\2\2\23\u00ae\3\2\2\2\25\u00b0\3\2\2\2\27\u00b2\3")
+        buf.write("\2\2\2\31\u00b4\3\2\2\2\33\u00b6\3\2\2\2\35\36\7F\2\2")
+        buf.write("\36\37\7G\2\2\37 \7E\2\2 !\7N\2\2!\"\7C\2\2\"#\7T\2\2")
+        buf.write("#$\7C\2\2$%\7E\2\2%&\7Q\2\2&\'\7G\2\2\'h\7U\2\2()\7C\2")
+        buf.write("\2)*\7N\2\2*+\7I\2\2+,\7Q\2\2,-\7T\2\2-.\7K\2\2./\7V\2")
+        buf.write("\2/\60\7O\2\2\60h\7Q\2\2\61\62\7K\2\2\62\63\7P\2\2\63")
+        buf.write("h\7V\2\2\64\65\7T\2\2\65\66\7G\2\2\66\67\7C\2\2\67h\7")
+        buf.write("N\2\289\7C\2\29:\7V\2\2:;\7T\2\2;<\7K\2\2<=\7D\2\2=>\7")
+        buf.write("W\2\2>?\7K\2\2?h\7T\2\2@h\7C\2\2AB\7N\2\2BC\7G\2\2Ch\7")
+        buf.write("T\2\2DE\7K\2\2EF\7O\2\2FG\7R\2\2GH\7T\2\2HI\7K\2\2IJ\7")
+        buf.write("O\2\2JK\7K\2\2Kh\7T\2\2LM\7U\2\2Mh\7G\2\2NO\7G\2\2OP\7")
+        buf.write("P\2\2PQ\7V\2\2QR\7C\2\2Rh\7Q\2\2ST\7G\2\2TU\7P\2\2UV\7")
+        buf.write("S\2\2VW\7W\2\2WX\7C\2\2XY\7P\2\2YZ\7V\2\2Zh\7Q\2\2[\\")
+        buf.write("\7K\2\2\\]\7P\2\2]^\7K\2\2^_\7E\2\2_`\7K\2\2`h\7Q\2\2")
+        buf.write("ab\7H\2\2bc\7K\2\2ch\7O\2\2dh\7G\2\2ef\7Q\2\2fh\7W\2\2")
+        buf.write("g\35\3\2\2\2g(\3\2\2\2g\61\3\2\2\2g\64\3\2\2\2g8\3\2\2")
+        buf.write("\2g@\3\2\2\2gA\3\2\2\2gD\3\2\2\2gL\3\2\2\2gN\3\2\2\2g")
+        buf.write("S\3\2\2\2g[\3\2\2\2ga\3\2\2\2gd\3\2\2\2ge\3\2\2\2h\4\3")
+        buf.write("\2\2\2ik\t\2\2\2ji\3\2\2\2jk\3\2\2\2km\3\2\2\2ln\4\62")
+        buf.write(";\2ml\3\2\2\2no\3\2\2\2om\3\2\2\2op\3\2\2\2p\6\3\2\2\2")
+        buf.write("qs\t\2\2\2rq\3\2\2\2rs\3\2\2\2su\3\2\2\2tv\4\62;\2ut\3")
+        buf.write("\2\2\2vw\3\2\2\2wu\3\2\2\2wx\3\2\2\2x\177\3\2\2\2y{\7")
+        buf.write("\60\2\2z|\4\62;\2{z\3\2\2\2|}\3\2\2\2}{\3\2\2\2}~\3\2")
+        buf.write("\2\2~\u0080\3\2\2\2\177y\3\2\2\2\177\u0080\3\2\2\2\u0080")
+        buf.write("\b\3\2\2\2\u0081\u0085\t\3\2\2\u0082\u0084\t\4\2\2\u0083")
+        buf.write("\u0082\3\2\2\2\u0084\u0087\3\2\2\2\u0085\u0083\3\2\2\2")
+        buf.write("\u0085\u0086\3\2\2\2\u0086\n\3\2\2\2\u0087\u0085\3\2\2")
+        buf.write("\2\u0088\u008d\7)\2\2\u0089\u008c\5\r\7\2\u008a\u008c")
+        buf.write("\n\5\2\2\u008b\u0089\3\2\2\2\u008b\u008a\3\2\2\2\u008c")
+        buf.write("\u008f\3\2\2\2\u008d\u008b\3\2\2\2\u008d\u008e\3\2\2\2")
+        buf.write("\u008e\u0090\3\2\2\2\u008f\u008d\3\2\2\2\u0090\u0091\7")
+        buf.write(")\2\2\u0091\f\3\2\2\2\u0092\u0093\7^\2\2\u0093\u0094\7")
+        buf.write(")\2\2\u0094\16\3\2\2\2\u0095\u0099\7\'\2\2\u0096\u0098")
+        buf.write("\n\6\2\2\u0097\u0096\3\2\2\2\u0098\u009b\3\2\2\2\u0099")
+        buf.write("\u0097\3\2\2\2\u0099\u009a\3\2\2\2\u009a\u009d\3\2\2\2")
+        buf.write("\u009b\u0099\3\2\2\2\u009c\u009e\7\17\2\2\u009d\u009c")
+        buf.write("\3\2\2\2\u009d\u009e\3\2\2\2\u009e\u009f\3\2\2\2\u009f")
+        buf.write("\u00a0\7\f\2\2\u00a0\u00a1\b\b\2\2\u00a1\20\3\2\2\2\u00a2")
+        buf.write("\u00a3\t\7\2\2\u00a3\u00a4\b\t\3\2\u00a4\22\3\2\2\2\u00a5")
+        buf.write("\u00af\7@\2\2\u00a6\u00a7\7@\2\2\u00a7\u00af\7?\2\2\u00a8")
+        buf.write("\u00af\7>\2\2\u00a9\u00aa\7>\2\2\u00aa\u00af\7?\2\2\u00ab")
+        buf.write("\u00ac\7>\2\2\u00ac\u00af\7@\2\2\u00ad\u00af\7?\2\2\u00ae")
+        buf.write("\u00a5\3\2\2\2\u00ae\u00a6\3\2\2\2\u00ae\u00a8\3\2\2\2")
+        buf.write("\u00ae\u00a9\3\2\2\2\u00ae\u00ab\3\2\2\2\u00ae\u00ad\3")
+        buf.write("\2\2\2\u00af\24\3\2\2\2\u00b0\u00b1\t\b\2\2\u00b1\26\3")
+        buf.write("\2\2\2\u00b2\u00b3\7<\2\2\u00b3\30\3\2\2\2\u00b4\u00b5")
+        buf.write("\7*\2\2\u00b5\32\3\2\2\2\u00b6\u00b7\7+\2\2\u00b7\34\3")
+        buf.write("\2\2\2\20\2gjorw}\177\u0085\u008b\u008d\u0099\u009d\u00ae")
+        buf.write("\4\3\b\2\3\t\3")
+        return buf.getvalue()
+
+
+class Analisador(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    PALAVRA_CHAVE = 1
+    NUMINT = 2
+    NUMREAL = 3
+    VARIAVEL = 4
+    CADEIA = 5
+    COMENTARIO = 6
+    WS = 7
+    OP_REL = 8
+    OP_ARIT = 9
+    DELIM = 10
+    ABREPAR = 11
+    FECHAPAR = 12
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "':'", "'('", "')'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "PALAVRA_CHAVE", "NUMINT", "NUMREAL", "VARIAVEL", "CADEIA", 
+            "COMENTARIO", "WS", "OP_REL", "OP_ARIT", "DELIM", "ABREPAR", 
+            "FECHAPAR" ]
+
+    ruleNames = [ "PALAVRA_CHAVE", "NUMINT", "NUMREAL", "VARIAVEL", "CADEIA", 
+                  "ESC_SEQ", "COMENTARIO", "WS", "OP_REL", "OP_ARIT", "DELIM", 
+                  "ABREPAR", "FECHAPAR" ]
+
+    grammarFileName = "Analisador.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.7.2")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
+    def action(self, localctx:RuleContext, ruleIndex:int, actionIndex:int):
+        if self._actions is None:
+            actions = dict()
+            actions[6] = self.COMENTARIO_action 
+            actions[7] = self.WS_action 
+            self._actions = actions
+        action = self._actions.get(ruleIndex, None)
+        if action is not None:
+            action(localctx, actionIndex)
+        else:
+            raise Exception("No registered action for:" + str(ruleIndex))
+
+    def COMENTARIO_action(self, localctx:RuleContext , actionIndex:int):
+        if actionIndex == 0:
+            skip();
+     
+
+    def WS_action(self, localctx:RuleContext , actionIndex:int):
+        if actionIndex == 1:
+            skip();
+     
+
+
---
+++ compilador/AnalisadorLexer.py
@@ -0,0 +1,136 @@
+# Generated from compilador/Analisador.g4 by ANTLR 4.7.2
+from antlr4 import *
+from io import StringIO
+from typing.io import TextIO
+import sys
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\16")
+        buf.write("\u0099\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+        buf.write("\4\16\t\16\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2")
+        buf.write("\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3")
+        buf.write("\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2")
+        buf.write("\3\2\3\2\3\2\3\2\3\2\5\2I\n\2\3\3\5\3L\n\3\3\3\6\3O\n")
+        buf.write("\3\r\3\16\3P\3\4\5\4T\n\4\3\4\6\4W\n\4\r\4\16\4X\3\4\3")
+        buf.write("\4\6\4]\n\4\r\4\16\4^\5\4a\n\4\3\5\3\5\7\5e\n\5\f\5\16")
+        buf.write("\5h\13\5\3\6\3\6\3\6\7\6m\n\6\f\6\16\6p\13\6\3\6\3\6\3")
+        buf.write("\7\3\7\3\7\3\b\3\b\7\by\n\b\f\b\16\b|\13\b\3\b\5\b\177")
+        buf.write("\n\b\3\b\3\b\3\b\3\t\3\t\3\t\3\n\3\n\3\n\3\n\3\n\3\n\3")
+        buf.write("\n\3\n\3\n\5\n\u0090\n\n\3\13\3\13\3\f\3\f\3\r\3\r\3\16")
+        buf.write("\3\16\2\2\17\3\3\5\4\7\5\t\6\13\7\r\2\17\b\21\t\23\n\25")
+        buf.write("\13\27\f\31\r\33\16\3\2\t\4\2--//\4\2C\\c|\5\2\62;C\\")
+        buf.write("c|\4\2))^^\4\2\f\f\17\17\5\2\13\f\17\17\"\"\5\2,-//\61")
+        buf.write("\61\2\u00ab\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2\2\2\2\t\3")
+        buf.write("\2\2\2\2\13\3\2\2\2\2\17\3\2\2\2\2\21\3\2\2\2\2\23\3\2")
+        buf.write("\2\2\2\25\3\2\2\2\2\27\3\2\2\2\2\31\3\2\2\2\2\33\3\2\2")
+        buf.write("\2\3H\3\2\2\2\5K\3\2\2\2\7S\3\2\2\2\tb\3\2\2\2\13i\3\2")
+        buf.write("\2\2\rs\3\2\2\2\17v\3\2\2\2\21\u0083\3\2\2\2\23\u008f")
+        buf.write("\3\2\2\2\25\u0091\3\2\2\2\27\u0093\3\2\2\2\31\u0095\3")
+        buf.write("\2\2\2\33\u0097\3\2\2\2\35\36\7c\2\2\36\37\7n\2\2\37 ")
+        buf.write("\7i\2\2 !\7q\2\2!\"\7t\2\2\"#\7k\2\2#$\7v\2\2$%\7o\2\2")
+        buf.write("%I\7q\2\2&\'\7f\2\2\'(\7g\2\2()\7e\2\2)*\7n\2\2*+\7c\2")
+        buf.write("\2+,\7t\2\2,I\7g\2\2-.\7n\2\2./\7k\2\2/\60\7v\2\2\60\61")
+        buf.write("\7g\2\2\61\62\7t\2\2\62\63\7c\2\2\63I\7n\2\2\64\65\7k")
+        buf.write("\2\2\65\66\7p\2\2\66\67\7v\2\2\678\7g\2\289\7k\2\29:\7")
+        buf.write("t\2\2:I\7q\2\2;<\7h\2\2<=\7k\2\2=>\7o\2\2>?\7a\2\2?@\7")
+        buf.write("c\2\2@A\7n\2\2AB\7i\2\2BC\7q\2\2CD\7t\2\2DE\7k\2\2EF\7")
+        buf.write("v\2\2FG\7o\2\2GI\7q\2\2H\35\3\2\2\2H&\3\2\2\2H-\3\2\2")
+        buf.write("\2H\64\3\2\2\2H;\3\2\2\2I\4\3\2\2\2JL\t\2\2\2KJ\3\2\2")
+        buf.write("\2KL\3\2\2\2LN\3\2\2\2MO\4\62;\2NM\3\2\2\2OP\3\2\2\2P")
+        buf.write("N\3\2\2\2PQ\3\2\2\2Q\6\3\2\2\2RT\t\2\2\2SR\3\2\2\2ST\3")
+        buf.write("\2\2\2TV\3\2\2\2UW\4\62;\2VU\3\2\2\2WX\3\2\2\2XV\3\2\2")
+        buf.write("\2XY\3\2\2\2Y`\3\2\2\2Z\\\7\60\2\2[]\4\62;\2\\[\3\2\2")
+        buf.write("\2]^\3\2\2\2^\\\3\2\2\2^_\3\2\2\2_a\3\2\2\2`Z\3\2\2\2")
+        buf.write("`a\3\2\2\2a\b\3\2\2\2bf\t\3\2\2ce\t\4\2\2dc\3\2\2\2eh")
+        buf.write("\3\2\2\2fd\3\2\2\2fg\3\2\2\2g\n\3\2\2\2hf\3\2\2\2in\7")
+        buf.write(")\2\2jm\5\r\7\2km\n\5\2\2lj\3\2\2\2lk\3\2\2\2mp\3\2\2")
+        buf.write("\2nl\3\2\2\2no\3\2\2\2oq\3\2\2\2pn\3\2\2\2qr\7)\2\2r\f")
+        buf.write("\3\2\2\2st\7^\2\2tu\7)\2\2u\16\3\2\2\2vz\7\'\2\2wy\n\6")
+        buf.write("\2\2xw\3\2\2\2y|\3\2\2\2zx\3\2\2\2z{\3\2\2\2{~\3\2\2\2")
+        buf.write("|z\3\2\2\2}\177\7\17\2\2~}\3\2\2\2~\177\3\2\2\2\177\u0080")
+        buf.write("\3\2\2\2\u0080\u0081\7\f\2\2\u0081\u0082\b\b\2\2\u0082")
+        buf.write("\20\3\2\2\2\u0083\u0084\t\7\2\2\u0084\u0085\b\t\3\2\u0085")
+        buf.write("\22\3\2\2\2\u0086\u0090\7@\2\2\u0087\u0088\7@\2\2\u0088")
+        buf.write("\u0090\7?\2\2\u0089\u0090\7>\2\2\u008a\u008b\7>\2\2\u008b")
+        buf.write("\u0090\7?\2\2\u008c\u008d\7>\2\2\u008d\u0090\7@\2\2\u008e")
+        buf.write("\u0090\7?\2\2\u008f\u0086\3\2\2\2\u008f\u0087\3\2\2\2")
+        buf.write("\u008f\u0089\3\2\2\2\u008f\u008a\3\2\2\2\u008f\u008c\3")
+        buf.write("\2\2\2\u008f\u008e\3\2\2\2\u0090\24\3\2\2\2\u0091\u0092")
+        buf.write("\t\b\2\2\u0092\26\3\2\2\2\u0093\u0094\7<\2\2\u0094\30")
+        buf.write("\3\2\2\2\u0095\u0096\7*\2\2\u0096\32\3\2\2\2\u0097\u0098")
+        buf.write("\7+\2\2\u0098\34\3\2\2\2\20\2HKPSX^`flnz~\u008f\4\3\b")
+        buf.write("\2\3\t\3")
+        return buf.getvalue()
+
+
+class AnalisadorLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    PALAVRA_CHAVE = 1
+    NUMINT = 2
+    NUMREAL = 3
+    VARIAVEL = 4
+    CADEIA = 5
+    COMENTARIO = 6
+    WS = 7
+    OP_REL = 8
+    OP_ARIT = 9
+    DELIM = 10
+    ABREPAR = 11
+    FECHAPAR = 12
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "':'", "'('", "')'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "PALAVRA_CHAVE", "NUMINT", "NUMREAL", "VARIAVEL", "CADEIA", 
+            "COMENTARIO", "WS", "OP_REL", "OP_ARIT", "DELIM", "ABREPAR", 
+            "FECHAPAR" ]
+
+    ruleNames = [ "PALAVRA_CHAVE", "NUMINT", "NUMREAL", "VARIAVEL", "CADEIA", 
+                  "ESC_SEQ", "COMENTARIO", "WS", "OP_REL", "OP_ARIT", "DELIM", 
+                  "ABREPAR", "FECHAPAR" ]
+
+    grammarFileName = "Analisador.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.7.2")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
+    def action(self, localctx:RuleContext, ruleIndex:int, actionIndex:int):
+        if self._actions is None:
+            actions = dict()
+            actions[6] = self.COMENTARIO_action 
+            actions[7] = self.WS_action 
+            self._actions = actions
+        action = self._actions.get(ruleIndex, None)
+        if action is not None:
+            action(localctx, actionIndex)
+        else:
+            raise Exception("No registered action for:" + str(ruleIndex))
+
+    def COMENTARIO_action(self, localctx:RuleContext , actionIndex:int):
+        if actionIndex == 0:
+            skip();
+     
+
+    def WS_action(self, localctx:RuleContext , actionIndex:int):
+        if actionIndex == 1:
+            skip();
+     
+
+
